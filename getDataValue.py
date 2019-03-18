f57f3816b92fb5ebeb31fa538b08ea22903868b2
Maxime C <maxime.castaing@telecomnancy.eu>
2019-03-18 11:50:09 +0100

    mise en forme module 3

--- getDataValue.py
+++ getDataValue.py
@@ -3,20 +3,18 @@ import os
  
 #create a gdb shell files to analyze UC variables
 def createDataValue(varinfo):
-    dtvFile = open("dataValue.sh", "w")
-    dtvFile.write("#!/bin/sh \n")
     for var in varinfo:
-        name= var[0]
-        lines= var[1]
+        function= var[0]
+        name= var[1]
+        lines= var[2]
+        dtvFile = open(function + "_" + name + "_dataValue.sh", "w")
+        dtvFile.write("#!/bin/sh \n")
         for line in lines:
             dtvFile.write("break " + line + "\n")
-    dtvFile.write("start \n continue")
-    for var in varinfo:
-        name= var[0]
-        lines= var[1]
+        dtvFile.write("start \n continue")
         for line in range(len(lines)):
             dtvFile.write("\n print " + name + "\n continue" )
-    dtvFile.close()
+        dtvFile.close()
     return 0
 
 
@@ -29,14 +27,14 @@ def get_vars_info_json():
     for v in vars:
         var=v["variable"]
         dico=var[0]
+        funname=v["function"]
         varname=dico["name"]
         varlines=dico["lines"]
-        info.append([varname,varlines])
+        info.append([funname,varname,varlines])
     return info
 
 def main():    
     listvar=get_vars_info_json()
-    print(listvar)
     createDataValue(listvar)
     return 0
 
